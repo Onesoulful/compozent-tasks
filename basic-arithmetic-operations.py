0f19ee9085332bc3182b1d15d61e8f4775b578fd
Onesoulful <166483782+Onesoulful@users.noreply.github.com>
2025-01-10 23:17:20 +0530

    Add files via upload

---
+++ basic-arithmetic-operations.py
@@ -0,0 +1,10 @@
+a= int(input("Enter the first number "))
+b= int(input("Enter the second number ")) 
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print(f'''The sum of two number is {c} 
+The difference between two number is {d} 
+The multiplication between two number is {e} 
+The Quotient is {f}''')
